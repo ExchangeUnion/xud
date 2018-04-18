8d8e4b398d3d6e8fc78b25ff81e16f3809a964e7
Moshe Shababo <interjoint1@gmail.com>
2018-04-18 17:03:43 +0300

    db automation, placeorder cli command

--- tasks/db/data/restart.js
+++
@@ -1,6 +0,0 @@
-const gulp = require('gulp');
-const runSequence = require('run-sequence');
-
-gulp.task('db.data.restart', (done) => {
-  runSequence('db.data.clear', 'db.data.populate', done);
-});
