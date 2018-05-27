2cb1a49caaa4023979d6d6c96a31ec86ffbf6faf
Moshe Shababo <interjoint1@gmail.com>
2018-05-27 19:01:01 +0300

    documentation, port type fix

--- lib/types/db.ts
+++ lib/types/db.ts
@@ -1,9 +1,18 @@
 import Sequelize, { DataTypeAbstract, DefineAttributeColumnOptions } from 'sequelize';
-import { db } from './index';
 
 export type SequelizeAttributes<T extends { [key: string]: any }> = {
   [P in keyof T]: string | DataTypeAbstract | DefineAttributeColumnOptions
 };
+
+/*
+* The following definitions are in sets of triplets, one for each Model (which represents a table in the database).
+*
+* "xFactory" is the type definition for the object which is required when a new record is to be created.
+*
+* "xAttributes" is the type definition of the record. It cannot support nullables, as it is being used for the table's columns definition.
+*
+* "xInstance" is the type definition of a fetched record as a Sequelize row instance, which contains some util properties.
+*/
 
 export type CurrencyFactory = {
   id: string;
@@ -15,7 +24,7 @@ export type CurrencyInstance = CurrencyAttributes & Sequelize.Instance<CurrencyA
 
 export type PeerFactory = {
   address: string;
-  port: string;
+  port: number;
 };
 
 export type PeerAttributes = PeerFactory & {
