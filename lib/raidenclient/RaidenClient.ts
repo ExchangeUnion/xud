f82b11ef8b2cf8a546201dee5d9a1845974fd3be
Daniel McNally <mcnallydp@gmail.com>
2018-05-30 13:08:42 -0400

    Add raiden wallet & channel methods (#93)
    * add raiden wallet & channel methods
    
    * use raiden-specific errors

--- lib/raidenclient/RaidenClient.ts
+++ lib/raidenclient/RaidenClient.ts
@@ -1,8 +1,26 @@
 import http from 'http';
-import assert from 'assert';
 
 import BaseClient, { ClientStatus } from'../BaseClient';
-import errors from './errors';
+import * as errors from './errors';
+
+/**
+ * A utility function to parse the payload from an http response.
+ */
+async function parseResponseBody<T>(res: http.IncomingMessage): Promise<T> {
+  res.setEncoding('utf8');
+  return new Promise<T>((resolve, reject) => {
+    let body: string = '';
+    res.on('data', (chunk) => {
+      body += chunk;
+    });
+    res.on('end', () => {
+      resolve(JSON.parse(body));
+    });
+    res.on('error', (err) => {
+      reject(err);
+    });
+  });
+}
 
 /**
  * The configurable options for the raiden client.
@@ -17,23 +35,34 @@ type RaidenClientConfig = {
  * The payload for the [[tokenSwap]] call.
  */
 type TokenSwapPayload = {
+  /** Either "maker" for initiating a swap or "taker" for filling one */
   role: string;
+  /** The amount being sent */
   sending_amount: number;
+  /** The identifier for the token being sent */
   sending_token: string;
+  /** The amount being received */
   receiving_amount: number;
+  /** The identifier for the token being received */
   receiving_token: string;
 };
 
 /**
- * Information about a raiden payment channel.
+ * The payload for the [[openChannel]] call.
  */
-type ChannelInfo = {
-  channel_address: string;
+type OpenChannelPayload = {
   partner_address: string;
   token_address: string;
   balance: number;
+  settle_timeout: 100;
+};
+
+/**
+ * A raiden payment channel.
+ */
+type Channel = OpenChannelPayload & {
+  channel_address: string;
   state: string;
-  settle_timeout: number;
 };
 
 /**
@@ -63,29 +92,53 @@ class RaidenClient extends BaseClient {
    * @param method An HTTP request method
    * @param payload The request payload
    */
-  private sendRequest(endpoint: string, method: string, payload?: object): Promise<http.IncomingMessage> {
-    if (this.isDisabled()) {
-      throw errors.RAIDEN_IS_DISABLED;
-    }
-    const options: http.RequestOptions = {
-      method,
-      hostname: this.host,
-      port: this.port,
-      path: `/api/1/${endpoint}`,
-    };
-
-    let payloadStr: string;
-    if (payload) {
-      payloadStr = JSON.stringify(payload);
-      options.headers = {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(payloadStr),
-      };
-    }
-
+  private sendRequest = (endpoint: string, method: string, payload?: object): Promise<http.IncomingMessage> => {
     return new Promise((resolve, reject) => {
-      const req: http.ClientRequest = http.request(options, (res) => {
-        resolve(res);
+      if (this.isDisabled()) {
+        reject(errors.RAIDEN_IS_DISABLED);
+      }
+
+      const options: http.RequestOptions = {
+        method,
+        hostname: this.host,
+        port: this.port,
+        path: `/api/1/${endpoint}`,
+      };
+
+      let payloadStr: string | undefined;
+      if (payload) {
+        payloadStr = JSON.stringify(payload);
+        options.headers = {
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(payloadStr),
+        };
+      }
+
+      const req = http.request(options, (res) => {
+        switch (res.statusCode) {
+          case 200:
+          case 201:
+          case 204:
+            resolve(res);
+            break;
+          case 402:
+            reject(errors.INSUFFICIENT_BALANCE);
+            break;
+          case 408:
+            reject(errors.TIMEOUT);
+            break;
+          case 409:
+            reject(errors.INVALID);
+            break;
+          case 500:
+            this.logger.error(`raiden server error ${res.statusCode}: ${res.statusMessage}`);
+            reject(errors.SERVER_ERROR);
+            break;
+          default:
+            this.logger.error(`unexpected raiden status ${res.statusCode}: ${res.statusMessage}`);
+            reject(errors.UNEXPECTED);
+            break;
+        }
       });
 
       req.on('error', (err) => {
@@ -103,51 +156,79 @@ class RaidenClient extends BaseClient {
   /**
    * Initiates or completes a Raiden token swap
    * @param target_address The address of the intended swap counterparty
-   * @param payload.role Either "maker" for initiating a swap or "taker" for filling one
-   * @param payload.sending_amount The amount being sent
-   * @param payload.sending_token The identifier for the token being sent
-   * @param payload.receiving_amount The amount being received
-   * @param payload.receiving_token The identifier for the token being received
+   * @param payload The token swap payload
    * @param identifier An identification number for this swap
    */
-  public async tokenSwap(target_address: string, payload: TokenSwapPayload, identifier: string): Promise<string> {
-    let endpoint: string = `token_swaps/${target_address}`;
+  public tokenSwap = async (target_address: string, payload: TokenSwapPayload, identifier: string): Promise<void> => {
+    let endpoint = `token_swaps/${target_address}`;
     if (identifier) {
       endpoint += `/${identifier}`;
     }
 
-    const res: any = await this.sendRequest(endpoint, 'PUT', payload);
-    if (res.statusCode === 201) {
-      return 'swap created';
-    } else {
-      throw new Error(`${res.statusCode}: ${res.statusMessage}`);
-    }
+    const res = await this.sendRequest(endpoint, 'PUT', payload);
   }
 
   /**
    * Get info about a given raiden payment channel.
+   * @param channel_address The address of the channel to query
    */
-  public async getChannelInfo(channel_address: string): Promise<ChannelInfo> {
-    assert(typeof channel_address === 'string', 'channel_address must be a string');
+  public getChannel = async (channel_address: string): Promise<Channel> => {
+    const endpoint = `channels/${channel_address}`;
+    const res = await this.sendRequest(endpoint, 'GET');
+    return parseResponseBody<Channel>(res);
+  }
 
-    const endpoint: string = `channels/${channel_address}`;
-    const res: any = await this.sendRequest(endpoint, 'GET', undefined);
+  /**
+   * Get info about all non-settled channels.
+   */
+  public getChannels = async (): Promise<[Channel]> => {
+    const endpoint = 'channels';
+    const res = await this.sendRequest(endpoint, 'GET');
+    return parseResponseBody<[Channel]>(res);
+  }
 
-    res.setEncoding('utf8');
-    return new Promise<ChannelInfo>((resolve, reject) => {
-      let body: string = '';
-      res.on('data', (chunk) => {
-        body += chunk;
-      });
-      res.on('end', () => {
-        resolve(JSON.parse(body));
-      });
-      res.on('error', (err) => {
-        reject(err);
-      });
-    });
+  /**
+   * Create a payment channel.
+   * @returns The channel_address for the newly created channel.
+   */
+  public openChannel = async (payload: OpenChannelPayload): Promise<string> => {
+    const endpoint = 'channels';
+    const res = await this.sendRequest(endpoint, 'PUT', payload);
+
+    const body = await parseResponseBody<{ channel_address }>(res);
+    return body.channel_address;
+  }
+
+  /**
+   * Close a payment channel.
+   * @param channel_address The address of the channel to close
+   */
+  public closeChannel = async (channel_address: string): Promise<void> => {
+    const endpoint = `channels/${channel_address}`;
+    const res = await this.sendRequest(endpoint, 'PATCH', { state: 'settled' });
+  }
+
+  /**
+   * Deposit more of a token to an existing channel
+   * @param channel_address The address of the channel to deposit to
+   * @param balance The amount to deposit to the channel
+   */
+  public depositToChannel = async(channel_address: string, balance: number): Promise<void> => {
+    const endpoint = `channels/${channel_address}`;
+    const res = await this.sendRequest(endpoint, 'PATCH', { balance });
+  }
+
+  /**
+   * Get the account address for the raiden node.
+   */
+  public getAddress = async (): Promise<string> => {
+    const endpoint = `address`;
+    const res = await this.sendRequest(endpoint, 'GET');
+
+    const body = await parseResponseBody<{ our_address }>(res);
+    return body.our_address;
   }
 }
 
 export default RaidenClient;
-export { RaidenClientConfig, TokenSwapPayload };
+export { RaidenClientConfig, TokenSwapPayload, OpenChannelPayload };
--- lib/raidenclient/errors.ts
+++ lib/raidenclient/errors.ts
@@ -1,11 +1,33 @@
 import errorCodesPrefix from '../constants/errorCodesPrefix';
 
 const codesPrefix = errorCodesPrefix.RAIDEN;
-const errors: any = {};
 
-errors.RAIDEN_IS_DISABLED = {
+export const RAIDEN_IS_DISABLED = {
   message: 'raiden is disabled',
   code: codesPrefix.concat('1'),
 };
 
-export default errors;
+export const INSUFFICIENT_BALANCE = {
+  message: 'insufficient balance to perform request',
+  code: codesPrefix.concat('2'),
+};
+
+export const TIMEOUT = {
+  message: 'request timed out',
+  code: codesPrefix.concat('3'),
+};
+
+export const INVALID = {
+  message: 'request is conflicting or otherwise invalid',
+  code: codesPrefix.concat('4'),
+};
+
+export const SERVER_ERROR = {
+  message: 'raiden server error',
+  code: codesPrefix.concat('5'),
+};
+
+export const UNEXPECTED = {
+  message: 'unexpected error during raiden request',
+  code: codesPrefix.concat('6'),
+};
