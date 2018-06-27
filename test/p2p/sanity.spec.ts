b1922ed0b37bede291796e913fe6b3d187d4564d
Balamurali Pandranki <balamurali@live.com>
2018-06-27 19:02:20 +0530

    revent port variable

--- test/p2p/sanity.spec.ts
+++ test/p2p/sanity.spec.ts
@@ -58,12 +58,12 @@ describe('P2P Sanity Tests', () => {
   });
 
   it('should return connected', async () => {
-    const result = await firstpeer.service.connect({ host:'localhost', port: secondpeerconfig.port });
+    const result = await firstpeer.service.connect({ host:'localhost', port: 8886 });
     expect(result).to.be.equal('Connected to peer (localhost:8886)');
   });
 
   it('should fail to connect', async () => {
-    const result = await firstpeer.service.connect({ host:'localhost', port: secondpeerconfig.port });
+    const result = await firstpeer.service.connect({ host:'localhost', port: 8886 });
     expect(result).to.be.equal('SocketAddress (localhost:8886) already connected');
   });
 
