22b86743a2965ae477478d7e969e2f08f63268e1
Balamurali Pandranki <balamurali@live.com>
2018-07-20 21:59:25 +0530

    Remove failing testcase has been fixed as part of #199

--- test/integration/OrderBook.spec.ts
+++ test/integration/OrderBook.spec.ts
@@ -39,15 +39,6 @@ describe('OrderBook', () => {
     await orderBook.init();
   });
 
-  async function getOrderQuantity(orderId: string): Promise<number> {
-    const result = await db.models.Order.find({
-      where: { id: orderId },
-      raw: true,
-      attributes: ['quantity'],
-    }) as db.OrderInstance;
-    return Number(result.quantity);
-  }
-
   it('should have pairs and matchingEngines equivalent loaded', () => {
     expect(orderBook.pairs).to.be.an('array');
     orderBook.pairs.forEach((pair) => {
@@ -59,16 +50,6 @@ describe('OrderBook', () => {
     const order: orders.OwnOrder = { pairId: 'BTC/LTC',  quantity: 5, price: 55 };
     await orderBook.addLimitOrder(order);
     await orderBook.addLimitOrder(order);
-  });
-
-  it('should match new ownOrder and update matches', async () => {
-    const order: orders.OwnOrder = { pairId: 'BTC/LTC', quantity: -6, price: 55 };
-    const matches = await orderBook.addLimitOrder(order);
-    expect(matches.remainingOrder).to.be.null;
-    const firstMakerLeft = await getOrderQuantity(matches.matches[0].maker.id);
-    expect(firstMakerLeft).to.be.equal(0);
-    const secondMakerLeft = await getOrderQuantity(matches.matches[1].maker.id);
-    expect(secondMakerLeft).to.be.equal(4);
   });
 
   after(async () => {
