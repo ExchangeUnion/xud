08910ed00b9f061112a68ab63b3b338800bb55fd
Daniel McNally <mcnallydp@gmail.com>
2018-07-30 20:04:26 -0400

    handle placeOrder errors
    This prevents the xud process from crashing due to an invalid `placeOrder` request, and also returns an informative error to the gRPC client with a proper status code.

--- lib/orderbook/errors.ts
+++ lib/orderbook/errors.ts
@@ -1,16 +1,21 @@
 import errorCodesPrefix from '../constants/errorCodesPrefix';
 
 const codesPrefix = errorCodesPrefix.ORDERBOOK;
-const errors: any = {};
+const errorCodes = {
+  INVALID_PAIR_ID: codesPrefix.concat('.1'),
+  DUPLICATED_ORDER: codesPrefix.concat('.2'),
+};
 
-errors.INVALID_PAIR_ID = pairId => ({
-  message: `invalid pairId: ${pairId}`,
-  code: codesPrefix.concat('1'),
-});
+const errors = {
+  INVALID_PAIR_ID: pairId => ({
+    message: `invalid pairId: ${pairId}`,
+    code: errorCodes.INVALID_PAIR_ID,
+  }),
+  DUPLICATED_ORDER: localId => ({
+    message: `duplicated localId: ${localId}`,
+    code: codesPrefix.concat('.2'),
+  }),
+};
 
-errors.DUPLICATED_ORDER = localId => ({
-  message: `duplicated localId: ${localId}`,
-  code: codesPrefix.concat('2'),
-});
-
+export { errorCodes };
 export default errors;
