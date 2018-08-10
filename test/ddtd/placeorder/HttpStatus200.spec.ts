c9d48a446e3087d12c2b24bfee59295f3ea6ddb2
programarivm <info@programarivm.com>
2018-08-10 12:27:27 +0100

    http://localhost:8080/api/v1/placeorder fails

--- test/ddtd/placeorder/HttpStatus200.spec.ts
+++ test/ddtd/placeorder/HttpStatus200.spec.ts
@@ -1,12 +1,31 @@
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import dev from '../constants/dev';
+import data from './data/http_status_200.json';
 
-const url = dev.protocol + '://' + dev.host + ':' + dev.port + '/api/' + dev.version + '/placeorder';
+const url = dev.protocol + '://' + dev.host + ':' + dev.port + '/api/' + dev.version;
+const action = '/placeorder';
 
-describe(url, () => {
+describe(url + action, () => {
   chai.use(chaiHttp);
-
-  // TODO
-
+  it('responds with http status 200', (done) => {
+    data.httpBody.forEach(function(httpBody) {
+      chai.request(url)
+        .post(action)
+        .set('content-type', 'application/json')
+        .send({
+          'pair_id': httpBody.pair_id,
+          'order_id': httpBody.order_id,
+          'price': httpBody.price,
+          'quantity': httpBody.quantity
+        })
+        .then((res: ChaiHttp.Response) => {
+          expect(res.status).to.equal(200);
+          expect(res).to.be.json;
+          done();
+        }, (err: any) => {
+          throw err;
+        });
+    });
+  });
 });
