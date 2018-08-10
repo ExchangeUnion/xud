3485f7a91c1fd14c30c77894a5b974eb9ec7273f
programarivm <info@programarivm.com>
2018-08-10 18:33:44 +0100

    structuring test folder

--- test/ddtd/http/cancelorder/HttpStatus200.spec.ts
+++ test/ddtd/http/cancelorder/HttpStatus200.spec.ts
@@ -1,10 +1,10 @@
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
-import dev from '../../../lib/constants/dev';
+import dev from '../../../../lib/constants/dev';
 import data from './data/http_status_200.json';
 
 const url = dev.protocol + '://' + dev.host + ':' + dev.port + '/api/' + dev.version;
-const action = '/placeorder';
+const action = '/cancelorder';
 
 describe(url + action, () => {
   chai.use(chaiHttp);
@@ -15,9 +15,7 @@ describe(url + action, () => {
         .set('content-type', 'application/json')
         .send({
           'pair_id': httpBody.pair_id,
-          'order_id': httpBody.order_id,
-          'price': httpBody.price,
-          'quantity': httpBody.quantity
+          'order_id': httpBody.order_id
         })
         .then((res: ChaiHttp.Response) => {
           expect(res.status).to.equal(200);
--- test/ddtd/http/placeorder/HttpStatus200.spec.ts
+++ test/ddtd/http/placeorder/HttpStatus200.spec.ts
@@ -1,10 +1,10 @@
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
-import dev from '../../../lib/constants/dev';
+import dev from '../../../../lib/constants/dev';
 import data from './data/http_status_200.json';
 
 const url = dev.protocol + '://' + dev.host + ':' + dev.port + '/api/' + dev.version;
-const action = '/cancelorder';
+const action = '/placeorder';
 
 describe(url + action, () => {
   chai.use(chaiHttp);
@@ -15,7 +15,9 @@ describe(url + action, () => {
         .set('content-type', 'application/json')
         .send({
           'pair_id': httpBody.pair_id,
-          'order_id': httpBody.order_id
+          'order_id': httpBody.order_id,
+          'price': httpBody.price,
+          'quantity': httpBody.quantity
         })
         .then((res: ChaiHttp.Response) => {
           expect(res.status).to.equal(200);
