bc136c1bf2b571b7775483ccab5cd69fb9fbbe54
programarivm <info@programarivm.com>
2018-08-11 12:14:01 +0100

    test/ddtd/grpc/getInfo/GrpcStatus0.spec.ts cleanup

--- test/ddtd/grpc/getInfo/GrpcStatus0.spec.ts
+++ test/ddtd/grpc/getInfo/GrpcStatus0.spec.ts
@@ -1,16 +1,22 @@
-import chai, { assert, expect } from 'chai';
+import chai, { assert } from 'chai';
 import grpc from 'grpc';
 import env from '../env';
 import { XudClient } from '../../../../lib/proto/xudrpc_grpc_pb';
+import { GetInfoRequest } from '../../../../lib/proto/xudrpc_pb';
 
 describe('client.getInfo()', () => {
   const credentials = grpc.credentials.createInsecure();
   let client = new XudClient(env.host + ':' + env.port, credentials);
 
   it('responds with grpc status 0', () => {
-    // client.getInfo();
-    // TODO
-    assert.isOk(false, 'TODO');
+      const request = new GetInfoRequest();
+      client.getInfo(request, (error: Error | null, response) => {
+        if (error) {
+          throw error;
+        } else if (response) {
+          assert.equal(0, response.toObject().code);
+        }
+      });
   });
 
 });
