dd0c42d5ef529cd7d052dd12d12f316bcfa51d6e
programarivm <info@programarivm.com>
2018-08-12 11:09:50 +0100

    bootstrapping Xud in ../test/ddtd/grpc/suite.ts and /test/ddtd/http/suite.ts

--- test/ddtd/grpc/suite.ts
+++ test/ddtd/grpc/suite.ts
@@ -1,3 +1,30 @@
+import Xud from '../../../lib/Xud';
+
+let xud: Xud;
+let config: any;
+
+config = {
+  webproxy: {
+    disable: false,
+    port: 8080,
+  },
+  lnd: {
+    disable: true,
+  },
+  raiden: {
+    disable: true,
+  },
+  db: {
+    database: 'xud_test',
+  },
+};
+
+xud = new Xud(config);
+
+xud.start();
+
 require('./getInfo/GrpcStatus0.spec.ts');
 require('./placeOrder/GrpcStatus0.spec.ts');
 require('./cancelOrder/GrpcStatus0.spec.ts');
+
+xud.shutdown();
--- test/ddtd/http/suite.ts
+++ test/ddtd/http/suite.ts
@@ -1,3 +1,30 @@
+import Xud from '../../../lib/Xud';
+
+let xud: Xud;
+let config: any;
+
+config = {
+  webproxy: {
+    disable: false,
+    port: 8080,
+  },
+  lnd: {
+    disable: true,
+  },
+  raiden: {
+    disable: true,
+  },
+  db: {
+    database: 'xud_test',
+  },
+};
+
+xud = new Xud(config);
+
+xud.start();
+
 require('./info/HttpStatus200.spec.ts');
 require('./placeorder/HttpStatus200.spec.ts');
 require('./cancelorder/HttpStatus200.spec.ts');
+
+xud.shutdown();
