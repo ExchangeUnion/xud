f11f9ff76c8ebf3718db058c80db66d976bb0020
Karl Ranna <karl@karlranna.com>
2018-10-24 16:18:55 +0300

    feat(cli): show table as getorders output

--- lib/cli/commands/getorders.ts
+++ lib/cli/commands/getorders.ts
@@ -1,6 +1,81 @@
 import { Arguments } from 'yargs';
 import { callback, loadXudClient } from '../command';
-import { GetOrdersRequest } from '../../proto/xudrpc_pb';
+import { GetOrdersRequest, GetOrdersResponse, Order, OrderSide } from '../../proto/xudrpc_pb';
+import Table, { HorizontalTable } from 'cli-table3';
+import colors from 'colors/safe';
+
+type FormatedTradingPairOrders = {
+  pairId: string,
+  orders: string[][],
+};
+
+const COLUMNS = [19, 19, 12, 19, 19, 12];
+const COLUMNS_IN_ORDER_SIDE = COLUMNS.length / 2;
+const HEADER = [
+  { content: colors.green('Buy'), colSpan: 3 },
+  { content: colors.red('Sell'), colSpan: 3 },
+];
+const SECONDARY_HEADER = [
+  colors.green('Quantity'),
+  colors.green('Price'),
+  colors.green('Own Order'),
+  colors.red('Quantity'),
+  colors.red('Price'),
+  colors.red('Own Order'),
+];
+
+const addSide = (orderSide: Order.AsObject[]): string[] => {
+  const order = orderSide.pop();
+  if (order) {
+    const isOwn = order.isOwnOrder ? 'X' : '';
+    return [
+      order.quantity.toFixed(8),
+      order.price.toString(),
+      isOwn,
+    ].map(i => order.isOwnOrder ? colors.cyan(i) : i);
+  } else {
+    return Array.from(Array(COLUMNS_IN_ORDER_SIDE)).map(() => '');
+  }
+};
+
+export const formatOrders = (orders: GetOrdersResponse.AsObject) => {
+  const formatedOrders: FormatedTradingPairOrders[] = [];
+  orders.ordersMap.forEach((tradingPair) => {
+    const buy = tradingPair[1].buyOrdersList;
+    const sell = tradingPair[1].sellOrdersList;
+    const totalRows = buy.length < sell.length
+      ? sell.length : buy.length;
+    const tradingPairOrders = Array.from(Array(totalRows))
+      .map(() => {
+        return addSide(buy).concat(addSide(sell));
+      });
+    formatedOrders.push({
+      pairId: tradingPair[0],
+      orders: tradingPairOrders,
+    });
+  });
+  return formatedOrders;
+};
+
+const createTable = () => {
+  const table = new Table({
+    colWidths: COLUMNS,
+  }) as HorizontalTable;
+  table.push(HEADER);
+  table.push(SECONDARY_HEADER);
+  return table;
+};
+
+const displayOrdersTable = (tradingPair: FormatedTradingPairOrders) => {
+  const table = createTable();
+  tradingPair.orders.forEach(order => table.push(order));
+  console.log(colors.underline(colors.bold(`\nTrading pair: ${tradingPair.pairId}`)));
+  console.log(table.toString());
+};
+
+const displayTables = (orders: GetOrdersResponse.AsObject) => {
+  formatOrders(orders).forEach(displayOrdersTable);
+};
 
 export const command = 'getorders [pair_id]';
 
@@ -17,5 +92,5 @@ export const handler = (argv: Arguments) => {
   const pairId = argv.pair_id ? argv.pair_id.toUpperCase() : undefined;
   request.setPairId(pairId);
   request.setIncludeOwnOrders(true);
-  loadXudClient(argv).getOrders(request, callback());
+  loadXudClient(argv).getOrders(request, callback(displayTables));
 };
--- test/unit/Command.spec.ts
+++ test/unit/Command.spec.ts
@@ -1,5 +1,7 @@
 import { expect } from 'chai';
 import { callback } from '../../lib/cli/command';
+import { formatOrders } from '../../lib/cli/commands/getorders';
+import { GetOrdersResponse } from '../../lib/proto/xudrpc_pb';
 
 describe('Command.callback', () => {
   it('should call displayTable callback when provided', () => {
@@ -17,5 +19,58 @@ describe('Command.callback', () => {
     /* tslint:disable */
     callback(displayTable)(null, mockGrpcResponse);
     expect(called).to.equal(true);
+  });
+});
+
+describe('Command.getorders.formatOrders', () => {
+  it('should flatten and format orders', () => {
+    const rawOrders = {
+      "ordersMap": [
+        [
+          "LTC/BTC",
+          {
+            "buyOrdersList": [
+              {
+                "price": 60,
+                "quantity": 1e-8,
+                "pairId": "LTC/BTC",
+                "id": "e7c89751-d6ce-11e8-8ab6-e145328fd9a5",
+                "peerPubKey": "03c57ca4c90a97dffbac5e6cb4488d42c43e359ef5647f7115595ba32d658613a5",
+                "localId": "",
+                "createdAt": 1540304477511,
+                "side": 0,
+                "isOwnOrder": false
+              }
+            ],
+            "sellOrdersList": [
+              {
+                "price": 120,
+                "quantity": 1e-8,
+                "pairId": "LTC/BTC",
+                "id": "e7ac5cc1-d6ce-11e8-b33c-53836c57c7be",
+                "peerPubKey": "",
+                "localId": "e7ac5cc0-d6ce-11e8-b33c-53836c57c7be",
+                "createdAt": 1540304477324,
+                "side": 1,
+                "isOwnOrder": true
+              }
+            ]
+          }
+        ]
+      ]
+    } as GetOrdersResponse.AsObject;
+    const output = formatOrders(rawOrders);
+    expect(output.length).to.equal(1);
+    expect(output[0].pairId).to.equal('LTC/BTC');
+    const expectedRow = [
+      '0.00000001',
+      '60',
+      '',
+      '\u001b[36m0.00000001\u001b[39m',
+      '\u001b[36m120\u001b[39m',
+      '\u001b[36mX\u001b[39m',
+    ];
+    expect(output[0].orders.length).to.equal(1);
+    expect(output[0].orders[0]).to.deep.equal(expectedRow);
   });
 });
