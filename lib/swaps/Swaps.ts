4add243458126ef06bd79908bf19d935c4d8f8fa
ImmanuelSegol <3ditds@gmail.com>
2018-10-29 12:34:32 +0200

    chore() remove obsolete SwapDeal

--- lib/swaps/Swaps.ts
+++ lib/swaps/Swaps.ts
@@ -12,56 +12,7 @@ import { StampedOwnOrder, StampedPeerOrder, SwapResult } from '../types/orders';
 import assert from 'assert';
 import { Models } from '../db/DB';
 import { SwapDealInstance } from 'lib/types/db';
-
-type SwapDeal = {
-  /** The role of the local node in the swap. */
-  role: SwapRole;
-  /** The most updated deal phase */
-  phase: SwapPhase;
-  /**
-   * The most updated deal state. State works together with phase to indicate where the
-   * deal is in its life cycle and if the deal is active, errored, or completed.
-   */
-  state: SwapState;
-  /** The reason for being in the current state. */
-  errorReason?: string;
-  /** The xud node pub key of the counterparty to this swap deal. */
-  peerPubKey: string;
-  /** The global order id in the XU network for the maker order being executed. */
-  orderId: string;
-  /** The local id of the own order involved in the swap. */
-  localId: string;
-  /** The quantity of the order to execute as proposed by the taker. */
-  proposedQuantity: number;
-  /** The accepted quantity of the order to execute as accepted by the maker. */
-  quantity?: number;
-  /** The trading pair of the order. The pairId together with the orderId are needed to find the deal in orderBook. */
-  pairId: string;
-  /** The number of satoshis (or equivalent) the taker is expecting to receive. */
-  takerAmount: number;
-  /** The currency the taker is expecting to receive. */
-  takerCurrency: string;
-  /** Taker's lnd pubkey on the taker currency's network. */
-  takerPubKey?: string;
-  /** The CLTV delta from the current height that should be used to set the timelock for the final hop when sending to taker. */
-  takerCltvDelta: number;
-  /** The number of satoshis (or equivalent) the maker is expecting to receive. */
-  makerAmount: number;
-  /** The currency the maker is expecting to receive. */
-  makerCurrency: string;
-  /** The CLTV delta from the current height that should be used to set the timelock for the final hop when sending to maker. */
-  makerCltvDelta?: number;
-  /** The price of the order that's being executed. */
-  price: number;
-  /** The hash of the preimage. */
-  r_hash: string;
-  r_preimage?: string;
-  /** The routes the maker should use to send to the taker. */
-  makerToTakerRoutes?: lndrpc.Route[];
-  createTime: number;
-  executeTime?: number;
-  completeTime?: number
-};
+import { SwapDeal } from './types';
 
 type OrderToAccept = {
   quantityToAccept: number;
@@ -169,7 +120,7 @@ class Swaps extends EventEmitter {
    */
   private persistDeal = async (deal: SwapDeal) => {
     if (this.usedHashes.has(deal.r_hash)) {
-      await this.repository.addSwapDeal(<SwapDealInstance>deal);
+      await this.repository.addSwapDeal(deal);
       this.removeDeal(deal);
     }
   }
