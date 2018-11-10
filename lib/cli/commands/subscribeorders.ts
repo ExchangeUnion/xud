6afbcacbf554b48055fc5d5fbbd55f25761e6fca
Immanuel Segol <3ditds@gmail.com>
2018-11-10 12:43:11 +0200

    chore(subscribeorders.ts) remove builder

--- lib/cli/commands/subscribeorders.ts
+++ lib/cli/commands/subscribeorders.ts
@@ -2,17 +2,9 @@ import { loadXudClient, callback } from '../command';
 import { Arguments } from 'yargs';
 import * as xudrpc from '../../proto/xudrpc_pb';
 
-export const command = 'streamorders [show_existing]';
+export const command = 'streamorders';
 
-export const describe = 'stream order added, removed, and swapped events (DEMO)';
-
-export const builder = {
-  show_existing: {
-    description: 'Return existing orders',
-    type: 'boolean',
-    default: false,
-  },
-};
+export const describe = 'stream orders existing added, removed, and swapped events (DEMO)';
 
 export const handler = (argv: Arguments) => {
   const addedOrdersSubscription = loadXudClient(argv).subscribeAddedOrders(new xudrpc.SubscribeAddedOrdersRequest());
