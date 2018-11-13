e501c5dc607c662a9e4726eb16da4b506fa8c829
Karl Ranna <karl@karlranna.com>
2018-11-13 17:46:03 +0200

    test(cli): update getorders test
    This commit updates getorders cli test to ensure that the orders are
    sorted as intended.

--- test/unit/Command.spec.ts
+++ test/unit/Command.spec.ts
@@ -3,54 +3,111 @@ import { formatOrders } from '../../lib/cli/commands/getorders';
 import { GetOrdersResponse } from '../../lib/proto/xudrpc_pb';
 
 describe('Command.getorders.formatOrders', () => {
-  it('should flatten and format orders', () => {
-    const rawOrders = {
+  it('should flatten, format and sort orders', () => {
+    const jsonOrders: GetOrdersResponse.AsObject = {
       ordersMap: [
         [
           'LTC/BTC',
           {
             buyOrdersList: [
               {
-                price: 60,
+                price: 0.004321,
                 quantity: 1e-8,
                 pairId: 'LTC/BTC',
-                id: 'e7c89751-d6ce-11e8-8ab6-e145328fd9a5',
-                peerPubKey: '03c57ca4c90a97dffbac5e6cb4488d42c43e359ef5647f7115595ba32d658613a5',
-                localId: '',
-                createdAt: 1540304477511,
+                id: '10270831-e755-11e8-a96e-13e8282e2780',
+                peerPubKey: '',
+                localId: '10270830-e755-11e8-a96e-13e8282e2780',
+                createdAt: 1542121316403,
                 side: 0,
-                isOwnOrder: false,
+                isOwnOrder: true,
+              },
+              {
+                price: 0.00321,
+                quantity: 1e-8,
+                pairId: 'LTC/BTC',
+                id: '107a5851-e755-11e8-a96e-13e8282e2780',
+                peerPubKey: '',
+                localId: '107a5850-e755-11e8-a96e-13e8282e2780',
+                createdAt: 1542121316949,
+                side: 0,
+                isOwnOrder: true,
               },
             ],
             sellOrdersList: [
               {
-                price: 120,
+                price: 0.00832,
                 quantity: 1e-8,
                 pairId: 'LTC/BTC',
-                id: 'e7ac5cc1-d6ce-11e8-b33c-53836c57c7be',
-                peerPubKey: '',
-                localId: 'e7ac5cc0-d6ce-11e8-b33c-53836c57c7be',
-                createdAt: 1540304477324,
+                id: '0f9af4d1-e755-11e8-8145-13a272c3a58d',
+                peerPubKey: '030130758847ada485520016a075833b8638c7e5a56889cb4b76e10c0f61f3520c',
+                localId: '',
+                createdAt: 1542121315487,
                 side: 1,
-                isOwnOrder: true,
+                isOwnOrder: false,
+              },
+              {
+                price: 0.00859,
+                quantity: 1e-8,
+                pairId: 'LTC/BTC',
+                id: '0fb6e141-e755-11e8-8145-13a272c3a58d',
+                peerPubKey: '030130758847ada485520016a075833b8638c7e5a56889cb4b76e10c0f61f3520c',
+                localId: '',
+                createdAt: 1542121315669,
+                side: 1,
+                isOwnOrder: false,
+              },
+              {
+                price: 0.00458,
+                quantity: 1e-8,
+                pairId: 'LTC/BTC',
+                id: '10b23131-e755-11e8-8145-13a272c3a58d',
+                peerPubKey: '030130758847ada485520016a075833b8638c7e5a56889cb4b76e10c0f61f3520c',
+                localId: '',
+                createdAt: 1542121317316,
+                side: 1,
+                isOwnOrder: false,
+              },
+              {
+                price: 0.00725,
+                quantity: 1e-8,
+                pairId: 'LTC/BTC',
+                id: '109644c1-e755-11e8-8145-13a272c3a58d',
+                peerPubKey: '030130758847ada485520016a075833b8638c7e5a56889cb4b76e10c0f61f3520c',
+                localId: '',
+                createdAt: 1542121317133,
+                side: 1,
+                isOwnOrder: false,
+              },
+              {
+                price: 0.00458,
+                quantity: 1e-8,
+                pairId: 'LTC/BTC',
+                id: '10ce92d1-e755-11e8-8145-13a272c3a58d',
+                peerPubKey: '030130758847ada485520016a075833b8638c7e5a56889cb4b76e10c0f61f3520c',
+                localId: '',
+                createdAt: 1542121317502,
+                side: 1,
+                isOwnOrder: false,
               },
             ],
           },
         ],
       ],
-    } as GetOrdersResponse.AsObject;
-    const output = formatOrders(rawOrders);
+    };
+    const output = formatOrders(jsonOrders);
     expect(output.length).to.equal(1);
     expect(output[0].pairId).to.equal('LTC/BTC');
-    const expectedRow = [
-      '0.00000001',
-      '60',
-      '',
+    const expectedFirstRow = [
       '\u001b[36m0.00000001\u001b[39m',
-      '\u001b[36m120\u001b[39m',
+      '\u001b[36m0.004321\u001b[39m',
       '\u001b[36mX\u001b[39m',
+      '0.00000001',
+      '0.00458',
+      '',
     ];
-    expect(output[0].orders.length).to.equal(1);
-    expect(output[0].orders[0]).to.deep.equal(expectedRow);
+    const expectedLastRow = ['', '', '', '0.00000001', '0.00859', ''];
+    expect(output[0].orders.length).to.equal(5);
+    expect(output[0].orders[0]).to.deep.equal(expectedFirstRow);
+    expect(output[0].orders[4]).to.deep.equal(expectedLastRow);
   });
 });
