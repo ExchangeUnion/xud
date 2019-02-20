a352e297aa13156fcb0343a9ad2b1135f3a1e96d
Daniel McNally <mcnallydp@gmail.com>
2019-02-19 23:43:27 -0500

    chore: remove protoFixDeprecation script
    This removes the `protoFixDeprecation` script as it is no longer needed
    with `grpc-tools` 1.7.0.

--- winFixDeprecation.ps1
+++
@@ -1,6 +0,0 @@
-$InputFiles = Get-Item "lib/proto/*.js"
-$OldString  = 'new Buffer'
-$NewString  = 'Buffer.from'
-$InputFiles | ForEach {
-    (Get-Content -Path $_.FullName).Replace($OldString,$NewString) | Set-Content -Path $_.FullName
-}
