d6e8aeec792302d5084db72301b164154e7cdfc8
Daniel McNally <mcnallydp@gmail.com>
2019-04-09 04:01:11 -0400

    chore: use existing protoc/xudrpc.proto for sim
    This modifies the gen_protos.sh script for the simulation tests to use
    the `protoc` executable that exists within the `node_modules` folder and
    which we already use to generate static protobuf files for xud. It also
    uses the existing xudrpc.proto file so that we don't need to maintain
    duplicate copies of the file within the repository.

--- test/simulation/xudrpc/gen_protos.sh
+++ test/simulation/xudrpc/gen_protos.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Generate the protos.
-protoc -I/usr/local/include -I. \
+../../../node_modules/grpc-tools/bin/protoc -I/usr/local/include -I../../../proto/ \
        -I$GOPATH/src \
        -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
        --go_out=plugins=grpc:. \
