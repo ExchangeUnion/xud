28dfae9f7a423d11cc248c4291eb244c098bbc79
Daniel McNally <mcnallydp@gmail.com>
2019-07-03 01:06:42 -0400

    fix: clean shutdown
    This removes unnecessary code that created duplicate swap client
    managers and interfered with the graceful shutdown procedure.

--- lib/Xud.ts
+++ lib/Xud.ts
@@ -108,9 +108,6 @@ class Xud extends EventEmitter {
 
       const initPromises: Promise<any>[] = [];
 
-      this.swapClientManager = new SwapClientManager(this.config, loggers);
-      initPromises.push(this.swapClientManager.init(this.db.models));
-
       this.swaps = new Swaps(loggers.swaps, this.db.models, this.pool, this.swapClientManager);
       initPromises.push(this.swaps.init());
 
