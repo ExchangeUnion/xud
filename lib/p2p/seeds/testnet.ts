616913d90c5338678cf907e1a434fbc03fe7adb5
Daniel McNally <mcnallydp@gmail.com>
2019-07-08 15:56:49 -0400

    feat: add testnet seed node
    Closes #1061.

--- lib/p2p/seeds/testnet.ts
+++ lib/p2p/seeds/testnet.ts
@@ -1,3 +1,8 @@
 import * as db from '../../db/types';
 
-export default [] as db.NodeAttributes[];
+export default [
+  {
+    nodePubKey: '02d8ac24fe2952d0d4c9eaa188a1d51ae4d80879fa311a3729a23a15b24f121d6c',
+    addresses: [{ host: 'xud1.testnet.exchangeunion.com', port: 8885 }],
+  },
+] as db.NodeAttributes[];
