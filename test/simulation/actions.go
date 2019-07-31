6266b9c5fa7ee752ef7d138bc42285f88ab9165a
Moshe Shababo <dicentralized@gmail.com>
2019-07-31 12:05:06 +0300

    test: breakswap (#1047)

--- test/simulation/actions.go
+++ test/simulation/actions.go
@@ -4,209 +4,13 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/ExchangeUnion/xud-simulation/lntest"
 	"github.com/ExchangeUnion/xud-simulation/xudrpc"
 	"github.com/ExchangeUnion/xud-simulation/xudtest"
+	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/stretchr/testify/require"
 	"time"
 )
-
-// testNetworkInit implements:
-// 1) Verify connectivity to swap clients of all nodes.
-// 2) Add currencies and pairs to all nodes.
-
-// testNetworkInit must be the first test case to be run, and the only one
-// allowed to alter the nodes critical state (peers, order book, etc.).
-// All following test cases must cleanup their changes before finishing
-// so that test cases would be stateless and won't be affected
-// by their preceding ones.
-func testNetworkInit(net *xudtest.NetworkHarness, ht *harnessTest) {
-	for _, node := range net.ActiveNodes {
-		// Verify connectivity.
-		timeout := time.Now().Add(10 * time.Second)
-		for {
-			req := &xudrpc.GetInfoRequest{}
-			res, err := node.Client.GetInfo(ht.ctx, req)
-
-			ht.assert.NoError(err)
-			ht.assert.NotNil(res.Lnd["BTC"])
-			ht.assert.NotNil(res.Lnd["LTC"])
-			if len(res.Lnd["BTC"].Chains) == 1 && len(res.Lnd["LTC"].Chains) == 1 {
-				ht.assert.Equal(res.Lnd["BTC"].Chains[0].Chain, "bitcoin")
-				ht.assert.Equal(res.Lnd["BTC"].Chains[0].Network, "simnet")
-				ht.assert.Equal(res.Lnd["LTC"].Chains[0].Chain, "litecoin")
-				ht.assert.Equal(res.Lnd["LTC"].Chains[0].Network, "simnet")
-				// Set the node public key.
-				node.SetPubKey(res.NodePubKey)
-				// Add pair to the node.
-				ht.act.addPair(node, "LTC", "BTC", xudrpc.AddCurrencyRequest_LND)
-				break
-			}
-			ht.assert.False(time.Now().After(timeout), "waiting for synced chains timeout")
-			// retry interval
-			time.Sleep(100 * time.Millisecond)
-		}
-	}
-}
-
-// testP2PDiscovery implements:
-// Connect Alice to Bob, Bob to Carol, Dave to Carol. After a short while, all of them should end up connected.
-func testP2PDiscovery(net *xudtest.NetworkHarness, ht *harnessTest) {
-	// Connect Alice to Bob.
-	ht.act.connect(net.Alice, net.Bob)
-	ht.act.verifyConnectivity(net.Alice, net.Bob)
-
-	// Connect Bob to Carol.
-	ht.act.connect(net.Bob, net.Carol)
-	ht.act.verifyConnectivity(net.Bob, net.Carol)
-
-	// Carol should hear about Alice from Bob,
-	// and should end up connected to her.
-	time.Sleep(1 * time.Second)
-	ht.act.verifyConnectivity(net.Alice, net.Carol)
-
-	// Connect Dave to Carol.
-	ht.act.connect(net.Dave, net.Carol)
-	ht.act.verifyConnectivity(net.Dave, net.Carol)
-
-	// Dave should hear about Alice and Bob from Carol,
-	// and should end up connected to them.
-	time.Sleep(1 * time.Second)
-	ht.act.verifyConnectivity(net.Alice, net.Dave)
-	ht.act.verifyConnectivity(net.Bob, net.Dave)
-
-	// Cleanup.
-	ht.act.disconnect(net.Alice, net.Bob)
-	ht.act.disconnect(net.Alice, net.Carol)
-	ht.act.disconnect(net.Alice, net.Dave)
-	ht.act.disconnect(net.Bob, net.Carol)
-	ht.act.disconnect(net.Bob, net.Dave)
-	ht.act.disconnect(net.Carol, net.Dave)
-}
-
-// testP2PIncorrectPubKey implements:
-// Alice connects to Bob expecting an incorrect public key.
-// Bob should reject the connection due to auth invalid target.
-func testP2PIncorrectPubKey(net *xudtest.NetworkHarness, ht *harnessTest) {
-	// Alice connects to Bob while expecting an incorrect public key.
-	incorrectPubKey := net.Bob.PubKey() + "Q"
-	destNodeURI := fmt.Sprintf("%v@%v", incorrectPubKey, net.Bob.Cfg.P2PAddr())
-	reqConn := &xudrpc.ConnectRequest{NodeUri: destNodeURI}
-
-	// Bob should reject the connection since Alice signed her handshake data
-	// specifying a different public key target then his.
-	_, err := net.Alice.Client.Connect(ht.ctx, reqConn)
-	ht.assert.Error(err)
-	ht.assert.Contains(err.Error(), fmt.Sprintf(
-		"Peer (%v) disconnected from us due to AuthFailureInvalidTarget", net.Bob.Cfg.P2PAddr()))
-}
-
-// testP2PBanUnban implements:
-// 1) If Alice ban Bob, connection attempts from both directions should fail.
-// 2) if Alice unban Bob, connection attempts from both directions should succeed.
-func testP2PBanUnban(net *xudtest.NetworkHarness, ht *harnessTest) {
-	// Alice to ban Bob.
-	ht.act.ban(net.Alice, net.Bob)
-
-	// Alice should not attempt to connect to Bob while he is banned.
-	reqConn := &xudrpc.ConnectRequest{NodeUri: net.Bob.NodeURI()}
-	_, err := net.Alice.Client.Connect(ht.ctx, reqConn)
-	ht.assert.Error(err)
-	ht.assert.Contains(err.Error(), fmt.Sprintf(
-		"could not connect to node %v because it is banned", net.Bob.PubKey()))
-
-	// If Bob connects to Alice, he should get rejected.
-	reqConn = &xudrpc.ConnectRequest{NodeUri: net.Alice.NodeURI()}
-	_, err = net.Bob.Client.Connect(ht.ctx, reqConn)
-	ht.assert.Error(err)
-	ht.assert.Contains(err.Error(), fmt.Sprintf(
-		" Peer (%v) disconnected from us due to Banned", net.Alice.Cfg.P2PAddr()))
-
-	// After Alice unban Bob, connection attempts from both directions should succeed.
-	ht.act.unban(net.Alice, net.Bob)
-	ht.act.connect(net.Alice, net.Bob)
-	ht.act.disconnect(net.Alice, net.Bob)
-	ht.act.connect(net.Bob, net.Alice)
-
-	// Cleanup.
-	ht.act.disconnect(net.Alice, net.Bob)
-}
-
-// testP2PAlreadyConnected implements:
-// If Alice and Bob are already connected, connection attempts from both directions should fail.
-func testP2PAlreadyConnected(net *xudtest.NetworkHarness, ht *harnessTest) {
-	// Connect Alice to Bob.
-	ht.act.connect(net.Alice, net.Bob)
-	ht.act.verifyConnectivity(net.Alice, net.Bob)
-
-	// Alice should not attempt to connect to Bob while they are already connected.
-	reqConn := &xudrpc.ConnectRequest{NodeUri: net.Bob.NodeURI()}
-	_, err := net.Alice.Client.Connect(ht.ctx, reqConn)
-	ht.assert.Error(err)
-	ht.assert.Contains(err.Error(), fmt.Sprintf(
-		"node %v at %v already connected", net.Bob.PubKey(), net.Bob.Cfg.P2PAddr()))
-
-	// Bob should not attempt to connect to Alice while they are already connected.
-	reqConn = &xudrpc.ConnectRequest{NodeUri: net.Alice.NodeURI()}
-	_, err = net.Bob.Client.Connect(ht.ctx, reqConn)
-	ht.assert.Error(err)
-	ht.assert.Contains(err.Error(), fmt.Sprintf(
-		"node %v at %v already connected", net.Alice.PubKey(), net.Alice.Cfg.P2PAddr()))
-
-	// Cleanup.
-	ht.act.disconnect(net.Alice, net.Bob)
-}
-
-// testOrderBroadcastAndInvalidation implements:
-// 1) Placed order should get broadcasted over the network, and added to connected peers' order books.
-// 2) Removed order should get invalidated over the network, and removed from connected peers' order books.
-func testOrderBroadcastAndInvalidation(net *xudtest.NetworkHarness, ht *harnessTest) {
-	// Connect Alice to Bob.
-	ht.act.connect(net.Alice, net.Bob)
-	ht.act.verifyConnectivity(net.Alice, net.Bob)
-
-	req := &xudrpc.PlaceOrderRequest{
-		Price:    0.02,
-		Quantity: 1000000,
-		PairId:   "LTC/BTC",
-		OrderId:  "random_order_id",
-		Side:     xudrpc.OrderSide_BUY,
-	}
-
-	order := ht.act.placeOrderAndBroadcast(net.Alice, net.Bob, req)
-	ht.act.removeOrderAndInvalidate(net.Alice, net.Bob, order)
-
-	// Cleanup.
-	ht.act.disconnect(net.Alice, net.Bob)
-}
-
-func testOrderMatchingAndSwap(net *xudtest.NetworkHarness, ht *harnessTest) {
-	// Connect Alice to Bob.
-	ht.act.connect(net.Alice, net.Bob)
-	ht.act.verifyConnectivity(net.Alice, net.Bob)
-
-	// Place an order on Alice.
-	req := &xudrpc.PlaceOrderRequest{
-		OrderId:  "maker_order_id",
-		Price:    0.02,
-		Quantity: 1000000,
-		PairId:   "LTC/BTC",
-		Side:     xudrpc.OrderSide_BUY,
-	}
-	ht.act.placeOrderAndBroadcast(net.Alice, net.Bob, req)
-
-	// Place a matching order on Bob.
-	req = &xudrpc.PlaceOrderRequest{
-		OrderId:  "taker_order_id",
-		Price:    req.Price,
-		Quantity: req.Quantity,
-		PairId:   req.PairId,
-		Side:     xudrpc.OrderSide_SELL,
-	}
-	ht.act.placeOrderAndSwap(net.Bob, net.Alice, req)
-
-	// Cleanup.
-	ht.act.disconnect(net.Alice, net.Bob)
-}
 
 // actions provide self-contained reusable scenarios to be used in tests.
 // each action performs its expected success/failure assertions by its own.
@@ -216,6 +20,34 @@ type actions struct {
 
 	// ctx is the context for the test scenario.
 	ctx context.Context
+}
+
+func (a *actions) init(node *xudtest.HarnessNode) {
+	// Verify connectivity.
+	timeout := time.Now().Add(10 * time.Second)
+	for {
+		req := &xudrpc.GetInfoRequest{}
+		res, err := node.Client.GetInfo(a.ctx, req)
+		a.assert.NoError(err)
+		a.assert.NotNil(res.Lnd["BTC"])
+		a.assert.NotNil(res.Lnd["LTC"])
+		if len(res.Lnd["BTC"].Chains) == 1 && len(res.Lnd["LTC"].Chains) == 1 {
+			a.assert.Equal(res.Lnd["BTC"].Chains[0].Chain, "bitcoin")
+			a.assert.Equal(res.Lnd["BTC"].Chains[0].Network, "simnet")
+			a.assert.Equal(res.Lnd["LTC"].Chains[0].Chain, "litecoin")
+			a.assert.Equal(res.Lnd["LTC"].Chains[0].Network, "simnet")
+
+			// Set the node public key.
+			node.SetPubKey(res.NodePubKey)
+
+			// Add pair to the node.
+			a.addPair(node, "LTC", "BTC", xudrpc.AddCurrencyRequest_LND)
+			break
+		}
+		a.assert.False(time.Now().After(timeout), "waiting for synced chains timeout")
+		// retry interval
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 
 func (a *actions) addPair(node *xudtest.HarnessNode, baseCurrency string, quoteCurrency string,
@@ -506,6 +338,44 @@ func subscribeSwaps(ctx context.Context, node *xudtest.HarnessNode, includeTaker
 	return out
 }
 
+type subscribeSwapFailuresEvent struct {
+	swapFailure *xudrpc.SwapFailure
+	err         error
+}
+
+func subscribeSwapFailures(ctx context.Context, node *xudtest.HarnessNode, includeTaker bool) <-chan *subscribeSwapFailuresEvent {
+	out := make(chan *subscribeSwapFailuresEvent, 1)
+
+	// Subscribe before starting a non-blocking routine.
+	req := xudrpc.SubscribeSwapsRequest{IncludeTaker: includeTaker}
+	stream, err := node.Client.SubscribeSwapFailures(ctx, &req)
+	if err != nil {
+		out <- &subscribeSwapFailuresEvent{nil, err}
+		return out
+	}
+
+	go func() {
+		go func() {
+			for {
+				swapFailure, err := stream.Recv()
+				out <- &subscribeSwapFailuresEvent{swapFailure, err}
+				if err != nil {
+					break
+				}
+			}
+		}()
+
+		select {
+		case <-ctx.Done():
+			if e := ctx.Err(); e != context.Canceled {
+				out <- &subscribeSwapFailuresEvent{nil, errors.New("timeout reached before event was received")}
+			}
+		}
+	}()
+
+	return out
+}
+
 func getOrdersCount(ctx context.Context, n1, n2 *xudtest.HarnessNode) (*xudrpc.OrdersCount, *xudrpc.OrdersCount, error) {
 	n1i, err := getInfo(ctx, n1)
 	if err != nil {
@@ -527,4 +397,101 @@ func getInfo(ctx context.Context, n *xudtest.HarnessNode) (*xudrpc.GetInfoRespon
 	}
 
 	return info, nil
+}
+
+func openBtcChannel(ctx context.Context, ln *lntest.NetworkHarness, srcNode, destNode *lntest.HarnessNode) (*lnrpc.ChannelPoint, error) {
+	openChanStream, err := ln.OpenChannel(ctx, srcNode, destNode, 15000000, 0, false)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := ln.BtcMiner.Node.Generate(6); err != nil {
+		return nil, err
+	}
+
+	return ln.WaitForChannelOpen(ctx, openChanStream)
+}
+
+func closeBtcChannel(ctx context.Context, ln *lntest.NetworkHarness, node *lntest.HarnessNode, cp *lnrpc.ChannelPoint, force bool) error {
+	closeChanStream, _, err := ln.CloseChannel(ctx, node, cp, force)
+	if err != nil {
+		return err
+	}
+
+	if _, err := ln.BtcMiner.Node.Generate(6); err != nil {
+		return err
+	}
+
+	if _, err := ln.WaitForChannelClose(ctx, closeChanStream); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func openLtcChannel(ctx context.Context, ln *lntest.NetworkHarness, srcNode, destNode *lntest.HarnessNode) (*lnrpc.ChannelPoint, error) {
+	openChanStream, err := ln.OpenChannel(ctx, srcNode, destNode, 15000000, 0, false)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := ln.LtcMiner.Node.Generate(6); err != nil {
+		return nil, err
+	}
+
+	return ln.WaitForChannelOpen(ctx, openChanStream)
+}
+
+func closeLtcChannel(ctx context.Context, ln *lntest.NetworkHarness, node *lntest.HarnessNode, cp *lnrpc.ChannelPoint, force bool) error {
+	closeChanStream, _, err := ln.CloseChannel(ctx, node, cp, force)
+	if err != nil {
+		return err
+	}
+
+	if _, err := ln.LtcMiner.Node.Generate(6); err != nil {
+		return err
+	}
+
+	if _, err := ln.WaitForChannelClose(ctx, closeChanStream); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type balance struct {
+	channel *lnrpc.ChannelBalanceResponse
+	wallet  *lnrpc.WalletBalanceResponse
+}
+
+type balances struct {
+	btc balance
+	ltc balance
+}
+
+func getBalance(ctx context.Context, node *xudtest.HarnessNode) (*balances, error) {
+	var b balances
+	var err error
+
+	b.ltc.channel, err = node.LndLtcNode.ChannelBalance(ctx, &lnrpc.ChannelBalanceRequest{})
+	if err != nil {
+		return nil, err
+	}
+
+	b.ltc.wallet, err = node.LndLtcNode.WalletBalance(ctx, &lnrpc.WalletBalanceRequest{})
+	if err != nil {
+		return nil, err
+	}
+
+	b.btc.channel, err = node.LndBtcNode.ChannelBalance(ctx, &lnrpc.ChannelBalanceRequest{})
+	if err != nil {
+		return nil, err
+	}
+
+	b.btc.wallet, err = node.LndBtcNode.WalletBalance(ctx, &lnrpc.WalletBalanceRequest{})
+	if err != nil {
+		return nil, err
+	}
+
+	return &b, nil
 }
--- test/simulation/go.mod
+++ test/simulation/go.mod
@@ -2,10 +2,12 @@ module github.com/ExchangeUnion/xud-simulation
 
 require (
 	github.com/Roasbeef/btcutil v0.0.0-20180406014609-dfb640c57141 // indirect
+	github.com/btcsuite/btcd v0.0.0-20190614013741-962a206e94e9
 	github.com/frankban/quicktest v1.2.2 // indirect
 	github.com/go-errors/errors v1.0.1
 	github.com/golang/protobuf v1.3.1
 	github.com/google/go-cmp v0.3.0 // indirect
+	github.com/kkdai/bstream v1.0.0 // indirect
 	github.com/lightningnetwork/lnd v0.6.1-beta
 	github.com/ltcsuite/ltcd v0.0.0-20190507171044-fbadf835b5c0
 	github.com/ltcsuite/ltcutil v0.0.0-20190507133322-23cdfa9fcc3d
@@ -14,10 +16,12 @@ require (
 	github.com/roasbeef/btcd v0.0.0-20180418012700-a03db407e40d
 	github.com/roasbeef/btcutil v0.0.0-20180406014609-dfb640c57141
 	github.com/stretchr/testify v1.3.0
-	golang.org/x/crypto v0.0.0-20190513172903-22d7a77e9e5f // indirect
-	golang.org/x/net v0.0.0-20190514140710-3ec191127204
-	golang.org/x/sys v0.0.0-20190514135907-3a4b5fb9f71f // indirect
+	golang.org/x/crypto v0.0.0-20190618222545-ea8f1a30c443 // indirect
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
+	golang.org/x/sys v0.0.0-20190620070143-6f217b454f45 // indirect
 	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/tools v0.0.0-20190620191750-1fa568393b23 // indirect
+	google.golang.org/appengine v1.4.0
 	google.golang.org/genproto v0.0.0-20190513181449-d00d292a067c
 	google.golang.org/grpc v1.20.1
 	gopkg.in/macaroon.v2 v2.1.0
