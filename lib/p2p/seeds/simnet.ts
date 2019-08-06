3c463272edd3e2aba47e49a044d40ad5b71f0f63
Kilian Rausch ⚡️ <kilian.rausch@gmail.com>
2019-08-06 10:49:46 +0200

    refactor: change xud simnet uris to follow naming convention (#1127)

--- lib/p2p/seeds/simnet.ts
+++ lib/p2p/seeds/simnet.ts
@@ -3,14 +3,14 @@ import * as db from '../../db/types';
 export default [
   {
     nodePubKey: '02b66438730d1fcdf4a4ae5d3d73e847a272f160fee2938e132b52cab0a0d9cfc6',
-    addresses: [{ host: 'xud1.test.exchangeunion.com', port: 8885 }],
+    addresses: [{ host: 'xud1.simnet.exchangeunion.com', port: 8885 }],
   },
   {
     nodePubKey: '028599d05b18c0c3f8028915a17d603416f7276c822b6b2d20e71a3502bd0f9e0a',
-    addresses: [{ host: 'xud2.test.exchangeunion.com', port: 8885 }],
+    addresses: [{ host: 'xud2.simnet.exchangeunion.com', port: 8885 }],
   },
   {
     nodePubKey: '03fd337659e99e628d0487e4f87acf93e353db06f754dccc402f2de1b857a319d0',
-    addresses: [{ host: 'xud3.test.exchangeunion.com', port: 8885 }],
+    addresses: [{ host: 'xud3.simnet.exchangeunion.com', port: 8885 }],
   },
 ] as db.NodeAttributes[];
