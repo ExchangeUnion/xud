28509ae1fdd61529585dbfa23696fb5128f1b626
Karl Ranna <karl@karlranna.com>
2019-08-26 18:31:58 +0000

    temp: simplify

--- test/simulation/geth.sh
+++ test/simulation/geth.sh
@@ -2,10 +2,14 @@
 set -xe
 declare -xp
 # export PATH="$PWD/go/bin:$PATH"
-export PATH="$PWD/go/bin:$PATH"
-export GOPATH="$PWD/go"
-GETH_PATH="$PWD/go/src/github.com/ethereum/go-ethereum"
-git clone --verbose https://github.com/ethereum/go-ethereum "$GETH_PATH"
-cd "$GETH_PATH" || exit 1
-echo "go version $(go version)"
-go run build/ci.go install
+# export PATH="$PWD/go/bin:$PATH"
+# export GOPATH="$PWD/go"
+# GETH_PATH="$PWD/go/src/github.com/ethereum/go-ethereum"
+# git clone --verbose https://github.com/ethereum/go-ethereum "$GETH_PATH"
+git clone https://github.com/ethereum/go-ethereum
+cd go-ethereum || exit 1
+git checkout v1.9.2
+# cd "$GETH_PATH" || exit 1
+# echo "go version $(go version)"
+# go run build/ci.go install
+make geth
