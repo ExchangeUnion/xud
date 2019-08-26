cd14f25c9c0d644ca71b11772731d35c00ce4a2e
Karl Ranna <karl@karlranna.com>
2019-08-26 18:19:56 +0000

    temp: asdf

--- test/simulation/geth.sh
+++ test/simulation/geth.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -xe
 declare -xp
+# export PATH="$PWD/go/bin:$PATH"
+export PATH="$PWD/go/bin:$PATH"
+export GOPATH="$PWD/go"
 GETH_PATH="$PWD/go/src/github.com/ethereum/go-ethereum"
 git clone --verbose https://github.com/ethereum/go-ethereum "$GETH_PATH"
 cd "$GETH_PATH" || exit 1
 echo "go version $(go version)"
-GO111MODULE=on make geth
+make geth
