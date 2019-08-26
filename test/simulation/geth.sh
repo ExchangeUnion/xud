112b06025a2556953f64c127abe3aa360c122c27
Karl Ranna <karl@karlranna.com>
2019-08-26 17:20:26 +0000

    temp: go version

--- test/simulation/geth.sh
+++ test/simulation/geth.sh
@@ -2,4 +2,5 @@
 GETH_PATH="$PWD/go/src/github.com/ethereum/go-ethereum"
 git clone --verbose https://github.com/ethereum/go-ethereum "$GETH_PATH"
 cd "$GETH_PATH" || exit 1
+echo "go version $(go version)"
 GO111MODULE=on make geth
