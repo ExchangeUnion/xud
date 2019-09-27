a5679fec60a12a06a3c6ddedd292222b430e834e
rsercano <ozdemirsercan27@gmail.com>
2019-09-27 14:22:54 +0200

    testing certificate read

--- test/simulation/xudtest/node.go
+++ test/simulation/xudtest/node.go
@@ -256,10 +256,12 @@ func (hn *HarnessNode) ConnectRPC(useMacs bool) (*grpc.ClientConn, error) {
 	var tries = 3
 	for tries > 0 {
 		if err != nil {
-			fmt.Printf("failed to create TLS from file with error: %v", err)
+			fmt.Printf("failed to create TLS from file with error: %v, remaining tries: %v\n", err, tries-1)
 			time.Sleep(3 * time.Second)
 			tlsCreds, err = credentials.NewClientTLSFromFile(hn.Cfg.TLSCertPath, "")
 			tries = tries - 1
+		} else {
+			break
 		}
 	}
 
