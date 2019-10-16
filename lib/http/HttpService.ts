41a541625d3551789f70036feaf614fdb2fb6f68
Daniel McNally <mcnallydp@gmail.com>
2019-10-17 01:10:14 +0200

    refactor(resolver): invalid arg if no secrethash

--- lib/http/HttpService.ts
+++ lib/http/HttpService.ts
@@ -1,16 +1,21 @@
 import Service from '../service/Service';
+import serviceErrors from '../service/errors';
 import { RaidenResolveRequest, RaidenResolveResponse } from '../raidenclient/types';
 
 class HttpService {
   constructor(private service: Service) {}
 
   public resolveHashRaiden = async (resolveRequest: RaidenResolveRequest): Promise<RaidenResolveResponse> => {
+    const { secrethash, amount, token, expiration, chain_height } = resolveRequest;
+    if (!secrethash || typeof secrethash !== 'string') {
+      throw serviceErrors.INVALID_ARGUMENT('secrethash must be a non-empty string');
+    }
     const secret = await this.service.resolveHash({
-      rHash: resolveRequest.secrethash.slice(2),
-      amount: resolveRequest.amount,
-      tokenAddress: resolveRequest.token,
-      expiration: resolveRequest.expiration,
-      chain_height: resolveRequest.chain_height,
+      amount,
+      expiration,
+      chain_height,
+      rHash: secrethash.slice(2),
+      tokenAddress: token,
     });
     return { secret };
   }
