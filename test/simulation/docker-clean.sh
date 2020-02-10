c37c77f1e0b05e780c182f04e699c97eb16d52af
Le Premier Homme <interjoint1@gmail.com>
2020-02-10 10:43:48 +0200

    docker-clean.sh to not return error status code

--- test/simulation/docker-clean.sh
+++ test/simulation/docker-clean.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 docker-compose down
-docker volume rm simulation_btcd-vol
-docker volume rm simulation_lnd-vol
-docker volume rm simulation_gomod-vol
+docker volume ls -q --filter "name=simulation_btcd-vol" | grep -q . && docker volume rm simulation_btcd-vol || echo "volume simulation_btcd-vol not found"
+docker volume ls -q --filter "name=simulation_lnd-vol" | grep -q . && docker volume rm simulation_lnd-vol || echo "volume simulation_lnd-vol not found"
+docker volume ls -q --filter "name=simulation_gomod-vol" |  grep -q . && docker volume rm simulation_gomod-vol || echo "volume simulation_gomod-vol not found"
+
+
