0739202d6c8a121d5165aff19dfbe57d116c98ed
Daniel McNally <mcnallydp@gmail.com>
2020-04-01 16:25:54 -0400

    test: add aliasUtils test cases

--- test/jest/aliasUtils.spec.ts
+++ test/jest/aliasUtils.spec.ts
@@ -1,17 +1,25 @@
 import { getAlias, isNodePubKey } from '../../lib/utils/aliasUtils';
 
 const nodePubKey = '028599d05b18c0c3f8028915a17d603416f7276c822b6b2d20e71a3502bd0f9e0a';
+const invalidPubKey = 'notarealnodepubkey028915a17d603416f7276c822b6b2d20e71a3502bd0f9e0a';
 const alias = 'SoccerFrost';
 
 describe('getAlias', () => {
   test('generates a deterministic alias from a node pub key', () => {
     expect(getAlias(nodePubKey)).toEqual(alias);
   });
+
+  test('throws an error when passed an invalid node pub key', () => {
+    expect(() => getAlias(invalidPubKey)).toThrow();
+  });
 });
 
 describe('isNodePubKey', () => {
-  test('indicates when a node identifier cannot be a node pub key', () => {
+  test('indicates that an alias cannot be a node pub key', () => {
     expect(isNodePubKey(alias)).toEqual(false);
+  });
+  test('indicates that an invalid string cannot be a node pub key', () => {
+    expect(isNodePubKey(invalidPubKey)).toEqual(false);
   });
   test('indicates when a node identifier could be a node pub key', () => {
     expect(isNodePubKey(nodePubKey)).toEqual(true);
