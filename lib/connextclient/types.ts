24247d9b4d54c4a26308c05267dfd7347401821c
Karl Ranna <karl@karlranna.com>
2020-04-13 18:43:36 +0000

    ConnextPreimageRequest under connext/types

--- lib/connextclient/types.ts
+++ lib/connextclient/types.ts
@@ -108,3 +108,13 @@ export type ExpectedIncomingTransfer = {
   expiry: number;
   tokenAddress: string;
 };
+
+export type ConnextPreimageRequest = {
+  id: string;
+  data: {
+    newState: {
+      lockHash: '0x88a2a1f394d7af4c2d403d7f02686451824f96c2829a24cf277b07b1633b17f0';
+      preImage: '0x358a4db68d6210ec55c80315acb10923738152bee25342fd02de9b220a5f793e';
+    };
+  }
+};
--- lib/http/HttpService.ts
+++ lib/http/HttpService.ts
@@ -2,16 +2,7 @@ import Service from '../service/Service';
 import serviceErrors from '../service/errors';
 import { TIMELOCK_BUFFER } from '../connextclient/ConnextClient';
 import { RaidenResolveRequest, RaidenResolveResponse } from '../raidenclient/types';
-
-type ConnextPreimageRequest = {
-  id: string;
-  data: {
-    newState: {
-      lockHash: '0x88a2a1f394d7af4c2d403d7f02686451824f96c2829a24cf277b07b1633b17f0';
-      preImage: '0x358a4db68d6210ec55c80315acb10923738152bee25342fd02de9b220a5f793e';
-    };
-  }
-};
+import { ConnextPreimageRequest } from '../connextclient/types';
 
 type ConnextIncomingTransferRequest = {
   id: string;
