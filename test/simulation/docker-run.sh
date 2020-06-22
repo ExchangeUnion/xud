d17ab6c66e612a72664b43298006c04b5abc01ec
Le Premier Homme <interjoint1@gmail.com>
2020-06-22 07:04:40 +0300

    gha debug

--- test/simulation/docker-run.sh
+++ test/simulation/docker-run.sh
@@ -7,6 +7,8 @@ then
     popd
 fi
 
+export DOCKER_CLIENT_TIMEOUT=120
+export COMPOSE_HTTP_TIMEOUT=120
 docker-compose run -v $PWD/temp/logs:/app/temp/logs test go test -v -timeout 20m -run=$@
 res=$?
 
