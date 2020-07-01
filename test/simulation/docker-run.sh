593fb847edd09daaea9a4029988d475454f62ca6
Le Premier Homme <interjoint1@gmail.com>
2020-07-01 12:59:58 +0300

    start indra in all modes

--- test/simulation/docker-run.sh
+++ test/simulation/docker-run.sh
@@ -1,27 +1,19 @@
 #!/usr/bin/env bash
 
-# create the temp directories with the current user so it is the owner for permissions
+# create the temp directory with the current user so it is the owner for permissions
 mkdir -p $PWD/temp/logs
-mkdir -p $PWD/temp/indra
 
-if [[ $@ == "TestIntegration" || $# == 0 ]]
-then
-    pushd temp/indra
-    make start
-    popd
-fi
+pushd temp/indra
+make start
+popd
 
 export DOCKER_CLIENT_TIMEOUT=120
 export COMPOSE_HTTP_TIMEOUT=120
 docker-compose run -v $PWD/temp/logs:/app/temp/logs test go test -v -timeout 20m -run=$@
 res=$?
 
-
-if [[ $@ == "TestIntegration" || $# == 0 ]]
-then
-    pushd temp/indra
-    make reset
-    popd
-fi
+pushd temp/indra
+make reset
+popd
 
 exit $res
