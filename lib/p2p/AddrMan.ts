47ea53c6050bb6c051f84ed2a750bd69e3458cdb
Hannah <ha237@cornell.edu>
2020-07-09 08:27:32 +0000

    progress

--- lib/p2p/AddrMan.ts
+++ lib/p2p/AddrMan.ts
@@ -3,6 +3,7 @@
 import assert from 'assert';
 import { NodeInstance } from '../db/types';
 import { createHash } from "crypto";
+//import Logger from '../Logger';
 
 class AddrInfo {
   public node = null;
@@ -21,7 +22,7 @@ class AddrInfo {
   // reference count in new sets
   private nRefCount = 0;
   // in tried set?
-  private fintried = false;
+  public fInTried = false;
   // position in vRandom
   private nRandomPos = -1;
 
@@ -29,7 +30,7 @@ class AddrInfo {
   public GetTriedBucket = (key: uint256): number => {
     /*hash this.node.lastAddress and key
     hash again
-    mod by AddrMan.ADDRMAN_TRIED_BUCKET_COUNT*/
+    mod by AddrMan.TRIED_BUCKET_COUNT*/
     return 0;
   }
   // Calculate in which "new" bucket this entry belongs, given a certain source
@@ -39,7 +40,7 @@ class AddrInfo {
   // Calculate in which position of a bucket to store this entry
   public GetBucketPosition = (key: uint256, fNew: boolean, nBucket: number): number => {
     let hash = createHash("sha256").update(key, 'Utf8').digest();
-    return hash % AddrMan.ADDRMAN_BUCKET_SIZE;
+    return hash % AddrMan.BUCKET_SIZE;
   }
 
   // Determine whether the statistics about this entry are bad enough so that it can just be deleted
@@ -51,13 +52,13 @@ class AddrInfo {
     if (this.nTime > time + 10 * 60) {
       return true;
     }
-    if (this.nTime == 0 || time - this.nTime > AddrMan.ADDRMAN_HORIZON_DAYS * 24 * 60 * 60) {
+    if (this.nTime == 0 || time - this.nTime > AddrMan.HORIZON_DAYS * 24 * 60 * 60) {
       return true;
     }
-    if (this.nLastSuccess == 0 && this.nAttempts >= AddrMan.ADDRMAN_RETRIES) {
+    if (this.nLastSuccess == 0 && this.nAttempts >= AddrMan.RETRIES) {
       return true;
     }
-    if (time - this.nLastSuccess > AddrMan.ADDRMAN_MIN_FAIL_DAYS * 24 * 60 * 60 && this.nAttempts >= AddrMan.ADDRMAN_MAX_FAILURES) {
+    if (time - this.nLastSuccess > AddrMan.MIN_FAIL_DAYS * 24 * 60 * 60 && this.nAttempts >= AddrMan.MAX_FAILURES) {
       return true;
     }
     return false;
@@ -73,16 +74,20 @@ class AddrInfo {
     fChance = pow(0.66, min(this.nAttempts, 8));
     return fChance;
   }
+  // Check if this address is routable
+  public IsRoutable = (): void => {
+
+  }
 
 }
 
 class AddrMan {
   // last used nId
   public nIdCount;
-  // table with informatino about all nIds
-  public mapInfo = new Map<number, NodeInstance>();
+  // table with information about all nIds
+  public mapAddr = new Map<number, AddrInfo>();
   // find an nId based on its network address
-  public mapAddr = new Map<NodeInstance, number>();
+  // public mapAddr = new Map<NodeInstance, number>();
   // randomly-ordered vector of all nIds
   public vRandom = [];
   // number of "tried" entries
@@ -98,68 +103,72 @@ class AddrMan {
   // secret key to randomize bucket select with
   private nKey;
   // total number of buckets for tried addresses
-  private static readonly ADDRMAN_TRIED_BUCKET_COUNT_LOG2 = 8;
+  private static readonly TRIED_BUCKET_COUNT_LOG2 = 8;
   // total number of buckets for new addresses
-  private static readonly ADDRMAN_NEW_BUCKET_COUNT_LOG2 = 10;
+  private static readonly NEW_BUCKET_COUNT_LOG2 = 10;
   // maximum allowed number of entries in buckets for new and tried addresses
-  private static readonly ADDRMAN_BUCKET_SIZE_LOG2 = 6;
+  private static readonly BUCKET_SIZE_LOG2 = 6;
   // over how many buckets entries with tried addresses from a single group  (/16 for IPv4) are spread
-  private static readonly ADDRMAN_TRIED_BUCKETS_PER_GROUP = 8;
+  private static readonly TRIED_BUCKETS_PER_GROUP = 8;
   // over how many buckets entries with new addresses originating from a single group are spread
-  private static readonly ADDRMAN_NEW_BUCKETS_PER_SOURCE_GROUP = 64;
+  private static readonly NEW_BUCKETS_PER_SOURCE_GROUP = 64;
   // in how many buckets for entries with new addresses a single address may occur
-  private static readonly ADDRMAN_NEW_BUCKETS_PER_ADDRESS = 8;
+  private static readonly NEW_BUCKETS_PER_ADDRESS = 8;
   // how old addresses can maximally be
-  public static readonly ADDRMAN_HORIZON_DAYS = 30;
+  public static readonly HORIZON_DAYS = 30;
   // after how many failed attempts we give up on a new node
-  public static readonly ADDRMAN_RETRIES = 3;
+  public static readonly RETRIES = 3;
   // how many successive failures are allowed ...
-  public static readonly ADDRMAN_MAX_FAILURES = 10;
+  public static readonly MAX_FAILURES = 10;
   // ... in at least this many days
-  public static readonly ADDRMAN_MIN_FAIL_DAYS = 7;
+  public static readonly MIN_FAIL_DAYS = 7;
   // how recent a successful connection should be before we allow an address to be evicted from tried
-  private static readonly ADDRMAN_REPLACEMENT_HOURS = 4;
+  private static readonly REPLACEMENT_HOURS = 4;
   // the maximum percentage of nodes to return in a getaddr call
-  private static readonly ADDRMAN_GETADDR_MAX_PCT = 23;
+  private static readonly GETADDR_MAX_PCT = 23;
   // the maximum number of nodes to return in a getaddr call
-  private static readonly ADDRMAN_GETADDR_MAX = 2500;
+  private static readonly GETADDR_MAX = 2500;
   // Convenience
-  private static readonly ADDRMAN_TRIED_BUCKET_COUNT = (1 << AddrMan.ADDRMAN_TRIED_BUCKET_COUNT_LOG2);
-  private static readonly ADDRMAN_NEW_BUCKET_COUNT = (1 << AddrMan.ADDRMAN_NEW_BUCKET_COUNT_LOG2);
-  private static readonly ADDRMAN_BUCKET_SIZE = (1 << AddrMan.ADDRMAN_BUCKET_SIZE_LOG2);
+  private static readonly TRIED_BUCKET_COUNT = (1 << AddrMan.TRIED_BUCKET_COUNT_LOG2);
+  private static readonly NEW_BUCKET_COUNT = (1 << AddrMan.NEW_BUCKET_COUNT_LOG2);
+  private static readonly BUCKET_SIZE = (1 << AddrMan.BUCKET_SIZE_LOG2);
   // the maximum number of tried addr collisions to store
-  private static readonly ADDRMAN_SET_TRIED_COLLISION_SIZE = 10;
+  private static readonly SET_TRIED_COLLISION_SIZE = 10;
   // the maximum time we'll spend trying to resolve a tried table collision, in seconds
-  private static readonly ADDRMAN_TEST_WINDOW = 40*60; // 40 minutes
+  private static readonly TEST_WINDOW = 40*60; // 40 minutes
 
   // list of "tried" buckets
-  public vvTried: AddrInfo[AddrMan.ADDRMAN_TRIED_BUCKET_COUNT][AddrMan.ADDRMAN_BUCKET_SIZE];
+  public vvTried: AddrInfo[AddrMan.TRIED_BUCKET_COUNT][AddrMan.BUCKET_SIZE];
   // list of "new" buckets
-  public vvNew = AddrInfo[AddrMan.ADDRMAN_NEW_BUCKET_COUNT][AddrMan.ADDRMAN_BUCKET_SIZE];
+  public vvNew = AddrInfo[AddrMan.NEW_BUCKET_COUNT][AddrMan.BUCKET_SIZE];
 
-  // Find an entry
-  public Find = (node: NodeInstance, pnId: number): AddrInfo => {
+  // constructor: logger, key, other vars?`
+
+  // Find an entry by url
+  public Find = (n: NodeInstance) => {
+    let toFind = `${n.node.lastAddress.host}:${n.node.lastAddress.port}`; 
     for (let [k,v] in this.mapAddr) {
-      if (v == node) { // === or == ?
-        return k;
+      let url = `${v.node.lastAddress.host}:${v.node.lastAddress.port}`;
+      if (url == toFind) { // === or == ?
+        return [k,v];
       }
     }
-    return null;
+    return [-1, null];
   }
-  // find an entry, creating it if necessary.
+  
   // nTime and nServices of the found node are updated, if necessary.
   public Create = (addr: NodeInstance, addrSource: NodeInstance, pnId: number): AddrInfo => {
     let nId = this.nIdCount++;
-    this.mapInfo[nId] = new AddrInfo();
-    this.mapInfo[nId].node = addr;
-    this.mapInfo[nId].source = addrSource;
-    this.mapAddr[addr] = nId;
-    this.mapInfo[nId].nRandomPos = this.vRandom.length;
+    let newAddr = new AddrInfo();
+    newAddr.node = addr;
+    newAddr.source = addrSource;
+    newAddr.nRandomPos = this.vRandom.length;
+    this.mapAddr.set(nId, newAddr);
     this.vRandom.push(nId);
     if (pnId) {
       this.pnId = pnId;
     }
-    return this.mapInfo[nId];
+    return newAddr;
   }
   // Swap two elements in vRandom
   public SwapRandom = (nRndPos1: number, nRndPos2: number): void => {
@@ -171,37 +180,140 @@ class AddrMan {
     let nId1 = this.vRandom[nRndPos1];
     let nId2 = this.vRandom[nRndPos2];
 
-    assert(this.mapInfo.has(nId1) && this.mapInfo.has(nId2));
+    assert(this.mapAddr.has(nId1) && this.mapAddr.has(nId2));
     
-    this.mapInfo[nId1].nRandomPos = nRndPos2;
-    this.mapInfo[nId2].nRandomPos = nRndPos1;
+    this.mapAddr[nId1].nRandomPos = nRndPos2;
+    this.mapAddr[nId2].nRandomPos = nRndPos1;
 
     this.vRandom[nRndPos1] = nId2;
     this.vRandom[nRndPos2] = nId1;
   }
-  // Move an entry from the "new" table = (s) to the "tried" table
-  public MakeTried = (info: NodeInstance, nId: number) => {
-    return 0;
+  // Move an entry from the "new" table to the "tried" table
+  public MakeTried = (nId: number) => {
+    // removed the entry from all new buckets
+    let entry = this.mapAddr.get(nId);
+    for (let bucket = 0; bucket < AddrMan.NEW_BUCKET_COUNT; bucket++) {
+      let pos = entry.GetBucketPosition(this.nKey, true, bucket);
+      if (this.vvNew[bucket][pos] == nId) {
+        this.vvNew[bucket][pos] = -1;
+        entry.nRefCount--;
+      }
+    }
+    this.nNew--;
+    
+    assert(entry.nRefCount == 0);
+
+    // which tried bucket to move the entry to
+    let nKBucket = entry.GetTriedBucket(this.nKey);
+    let nKBucket = entry.GetBucketPosition(this.nKey, false, nKBucket);
+
+    // first make space to add it (the existing tried entry there is moved to new, deleting whatever is there).
+    if (this.vvTried[nKBucket][nKBucketPos] !== -1) {
+      // find and item to evict
+      let nIdEvict = this.vvTried[nKBucket][nKBucketPos];
+      assert(this.mapAddr.count(nIdEvict) == 1);
+      let entryOld = this.mapAddr.get(nIdEvict);
+
+      // Remove the to-be-evicted item from the tried set.
+      entryOld.fInTried = false;
+      this.vvTried[nKBucket][nKBucket] = -1;
+      this.nTried--;
+
+      // find which new bucket it belongs to
+      let nUBucket = infoOld.GetNewBucket(this.nKey);
+      let nUBucket = infoOld.GetBucketPosition(this.nKey, true, nUBucket);
+      this.ClearNew(nUBucket, nUBucketPos);
+      assert(this.vvNew[nUBucket][nUBucketPos]) == -1;
+
+      // Enter it into the new set again
+      entryOld.nRefCount = 1;
+      this.mapAddr.set(nIdEvict, entryOld);
+      this.vvNew[nUBucket][nUBucketPos] = nIdEvict;
+      this.nNew++;
+    }
+    assert(this.vvTried[nKBucket][nKBucketPos] == -1);
+
+    this.vvTried[nKBucket][nKBucketPos] = nId;
+    this.nTried++;
+    entry.fInTried = true; 
+    this.mapAddr.set(nId, entry);
   }
+
   // Delete an entry. It must not be in tried, and have refcount 0
   public Delete = (nId: number): void => {
-    assert(this.mapInfo.has(nId));
-    let info = this.mapInfo[nId];
-    assert(!info.fInTried);
-    assert(info.nRefCount === 0);
+    assert(this.mapAddr.has(nId));
+    let entry = this.mapAddr.get(nId);
+    assert(!entry.fInTried);
+    assert(entry.nRefCount === 0);
 
-    this.SwapRandom(info.nRandomPos, vRandom.length - 1);
+    this.SwapRandom(entry.nRandomPos, this.vRandom.length - 1);
     this.vRandom.pop();
-    this.mapAddr.delete(info);
-    this.mapInfo.delete(nId);
+    this.mapAddr.delete(nId);
     this.nNew--;
   }
   // Clear a position in a "new" table. This is the only place where entries are actually deleted
   public ClearNew = (nUBucket: number, nUBucketPos: number): void => {
-
+    // if there is an entry in the specified bucket, delete it
+    if (this.vvNew[nUBucket][nUBucketPos] !== -1) {
+      let nIdDelete = this.vvNew[nUBucket][nUBucketPos];
+      let entryDelete = this.mapAddr.get(nIdDelete);
+      assert(entryDelete.nRefCount > 0);
+      entryDelete.nRefCount--;
+      this.mapAddr.set(nIdDelete, entryDelete);
+      this.vvNew[nUBucket][nUBucketPos] = -1;
+      if (entryDelete.nRefCount == 0) {
+        this.Delete(nIdDelete);
+      }
+    }
   }
   // Mark an entry "good", possibly moving it from "new" to "tried"
-  public Good = (addr: NodeInstance, test_before_evict: boolean, time: number): void => {
+  public Good = (addr: NodeInstance, test_before_evict: boolean, nTime: number): void => {
+    
+    this.nLastGood = nTime;
+    let [nId, entry] = this.Find(addr);
+
+    if (!(nId && info)) {
+      return;
+    }
+
+    if (entry.fInTried) {
+      return;
+    }
+    
+    entry.nLastSuccess = nTime;
+    entry.nLastTry = nTime;
+    entry.nAttempts = 0;
+    this.mapAddr.set(nId, entry);
+
+    // find a bucket it is in now
+    let nRnd = this.getRandomInt(AddrMan.NEW_BUCKET_COUNT);
+    let nUBucket = -1;
+    for (let n = 0; n < AddrMan.NEW_BUCKET_COUNT; n++) {
+      let nB = (n + nRnd) % AddrMan.NEW_BUCKET_COUNT;
+      let nBpos = entry.GetBucketPosition(this.nKey, true, nB);
+      if (this.vvNew[nB][nBpos] == nId) {
+        nUBucket = nB;
+        break;
+      }
+    }
+    // if no bucket is found, something bad happened
+    if (nUBucket == -1) {
+      return;
+    }
+
+    // which tried bucket to move the entry to
+    let tried_bucket = entry.GetTriedBucket(this.nKey);
+    let tried_bucket_pos = entry.GetBucketPosition(this.nKey, false, tried_bucket);
+    if (test_before_evict && (this.vvTried[tried_bucket][tried_bucket_pos] !== -1)) {
+      let colliding_entry = this.mapAddr.find(this.vvTried[tried_bucket][tried_bucket_pos]);
+      // TODO this.logger.info(`Collision inserting element...`);
+      if (this.m_tried_collisions.length < AddrMan.SET_TRIED_COLLISION_SIZE) {
+        this.m_tried_collisions.push(nId);
+      }
+    } else {
+      // this.logger.info(`moving to ...`);
+      this.MakeTried(nId);
+    }
   }
   // TODO Add an entry to the "new" table
   public Add = (addr: NodeInstance, source: NodeInstance, nTimePenalty: number): boolean => {
@@ -212,8 +324,23 @@ class AddrMan {
   }
   // Mark and entry as attempted to connect
   public Attempt = (addr: NodeInstance, fCountFailure: boolean, nTime: number): void => {
+    let [nId, info] = this.Find(addr);
+
+    if (!(nId && info)) {
+      return;
+    }
+    if (info !== addr) {
+      return;
+    }
+    info.nLastTry = nTime;
+    if (fCountFailure && info.nLastCountAttempt < this.nLastGood) {
+      info.nLastCountAttempt = nTime;
+      info.nAttempts++;
+    }
+    this.mapAddr.set(nId, info);
   }
-  private getRandomInt(max) {
+
+  private getRandomInt(max: number) {
     return Math.floor(Math.random() * Math.floor(max));
   }
   // Select an address to connect to, if newOnly is set to true, only the new table is selected from
@@ -227,14 +354,43 @@ class AddrMan {
     if (!newOnly && (this.nTried > 0 && (this.nNew === 0 || Math.random() < 0.5))) {
       let fChanceFactor = 1.0;
       while (true) {
-        let nKBucket = this.getRandomInt(AddrMan.ADDRMAN_TRIED_BUCKET_COUNT);
-        let nKBucketPos = this.getRandomInt(AddrMan.ADDRMAN_BUCKET_SIZE);
+        let nKBucket = this.getRandomInt(AddrMan.TRIED_BUCKET_COUNT);
+        let nKBucketPos = this.getRandomInt(AddrMan.BUCKET_SIZE);
         while (this.vvTried[nKBucket][nKBucketPos] == -1) {
-          nKBucket = (nKBucket + 
-    return new AddrInfo();
+          nKBucket = (nKBucket + this.getRandomInt(AddrMan.TRIED_BUCKET_COUNT)) % AddrMan.TRIED_BUCKET_COUNT;
+          nKBucketPos = (nKBucketPos + this.getRandomInt(AddrMan.BUCKET_SIZE)) % AddrMan.BUCKET_SIZE;
+        }
+        let nId = this.vvTried[nKBucket][nKBucketPos];
+        assert(this.mapAddr.count(nId) == 1);
+        let info = this.mapAddr.get(nId);
+        if (this.getRandomInt(1 << 30) < fChanceFactor * info.GetChance() * (1 << 30)) {
+          return info;
+        }
+        fChanceFactor *= 1.2;
+      }
+    } else {
+       let fChanceFactor = 1.0;
+      while (true) {
+        let nKBucket = this.getRandomInt(AddrMan.NEW_BUCKET_COUNT);
+        let nKBucketPos = this.getRandomInt(AddrMan.BUCKET_SIZE);
+        while (this.vvNew[nKBucket][nKBucketPos] == -1) {
+          nKBucket = (nKBucket + this.getRandomInt(AddrMan.NEW_BUCKET_COUNT)) % AddrMan.NEW_BUCKET_COUNT;
+          nKBucketPos = (nKBucketPos + this.getRandomInt(AddrMan.BUCKET_SIZE)) % AddrMan.BUCKET_SIZE;
+        }
+        let nId = this.vvNew[nKBucket][nKBucket];
+        assert(this.mapAddr.count(nId) == 1);
+        let info = this.mapAddr.get(nId);
+        if (this.getRandomInt(1 << 30) < fChanceFactor * info.GetChance() * (1 << 30)) {
+          return info;
+        }
+        fChanceFactor *= 1.2;
+
+      }
+    }
   }
   // See if any to-be-evicted tried table entries have been tested and if so resolve the collisions
   public ResolveCollisions = (): void => {
+    return;
   }
   // Return a random to-be-evicted tried table address
   public SelectTriedCollision = (): AddrInfo => {
