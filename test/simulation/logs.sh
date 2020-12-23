6447152623ed3e24a8fe676006c9067ba3401301
Daniel McNally <mcnallydp@gmail.com>
2020-12-22 19:45:05 -0500

    chore: output lnd logs for sim tests

--- test/simulation/logs.sh
+++ test/simulation/logs.sh
@@ -2,3 +2,4 @@
 
 find $PWD/temp/logs -type f -name xud*.log -printf "\n%f\n\n" -exec cat {} \;
 find $PWD/temp/logs -type f -name connext*.log -printf "\n%f\n\n" -exec cat {} \;
+find $PWD/temp/logs -type f -name lnd*.log -printf "\n%f\n\n" -exec cat {} \;
